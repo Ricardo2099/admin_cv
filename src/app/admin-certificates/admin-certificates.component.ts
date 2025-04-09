1e056f9c40e99245bff0c639f70dcda8e6d94003
Ricardo2099 <ricardorojas104@gmail.com>
2025-04-09 15:31:32 -0600

    agregando cosas

--- src/app/admin-certificates/admin-certificates.component.ts
+++ src/app/admin-certificates/admin-certificates.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { Certificate } from '../models/certificates/certificates.model';
+import { map } from 'rxjs/operators';
+import { CertificatesService } from '../services/certificates-service/certificate.service';
 
 @Component({
   selector: 'app-admin-certificates',
   templateUrl: './admin-certificates.component.html',
-  styleUrl: './admin-certificates.component.css'
+  styleUrls: ['./admin-certificates.component.css']
 })
 export class AdminCertificatesComponent {
+  itemTitle = 'Agregar';
+  btnTxt = 'Agregar';
+  myCertificate: Certificate = {
+    certificacion: '',
+    fechaObtencion: '',
+    institucion: ''
+  };
+  certificates?: Certificate[];
 
+  constructor(public certificatesService: CertificatesService) {
+    this.certificatesService.getCertificates()
+      .snapshotChanges()
+      .pipe(
+        map((changes: any[]) =>
+          changes.map((c: any) => ({
+            id: c.payload.doc.id,
+            ...c.payload.doc.data() as Certificate
+          }))
+        )
+      )
+      .subscribe((data: Certificate[]) => {
+        this.certificates = data;
+      });
+  }
+
+  AgregarCertificate() {
+    this.certificatesService.createCertificates(this.myCertificate).then(() => {
+      this.myCertificate = {
+        certificacion: '',
+        fechaObtencion: '',
+        institucion: ''
+      };
+    });
+  }
+
+  deleteCertificate(id?: string) {
+    if (!id) return;
+    this.certificatesService.deleteCertificates(id).then(() => {
+      console.log('Item eliminado correctamente!');
+    });
+  }
 }
--- src/app/services/certificates-service/certificate.service.ts
+++ src/app/services/certificates-service/certificate.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Certificate } from '../../models/certificates/certificates.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CertificateService {
+export class CertificatesService {
+  private dbPath = '/certificates';
+  certificatesRef: AngularFirestoreCollection<Certificate>;
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+    this.certificatesRef = db.collection(this.dbPath);
+  }
+
+  getCertificates(): AngularFirestoreCollection<Certificate> {
+    return this.certificatesRef;
+  }
+
+  createCertificates(certificate: Certificate): any {
+    return this.certificatesRef.add({ ...certificate });
+  }
+
+  deleteCertificates(id: string): Promise<void> {
+    return this.certificatesRef.doc(id).delete();
+  }
 }

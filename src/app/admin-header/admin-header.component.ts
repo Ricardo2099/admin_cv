120ca3f731b75d66b0e511b7014fbf03e190e78f
Ricardo2099 <ricardorojas104@gmail.com>
2025-04-10 00:51:27 -0600

    mas mejoras

--- src/app/admin-header/admin-header.component.ts
+++ src/app/admin-header/admin-header.component.ts
@@ -1,10 +1,94 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Header } from '../models/header/header.model';
+import { HeaderService } from '../services/header-service/header.service';
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './admin-header.component.html',
-  styleUrl: './admin-header.component.css'
+  styleUrls: ['./admin-header.component.css']
 })
 export class AdminHeaderComponent {
+  itemTitle = 'Agregar';
+  btnTxt = 'Agregar';
+  isEditing = false;
+  editingId: string | null = null;
 
+  myHeader: Header = {
+    email: '',
+    goalLife: '',
+    location: '',
+    name: '',
+    phoneNumber: '',
+    photoUrl: '',
+    socialNetwork: ''
+  };
+
+  headers?: Header[];
+
+  constructor(public headerService: HeaderService) {
+    this.headerService.getHeaders()
+      .snapshotChanges()
+      .pipe(
+        map((changes: any[]) =>
+          changes.map((c: any) => ({
+            id: c.payload.doc.id,
+            ...c.payload.doc.data() as Header
+          }))
+        )
+      )
+      .subscribe((data: Header[]) => {
+        this.headers = data;
+      });
+  }
+
+  AgregarHeader() {
+    this.headerService.createHeader(this.myHeader).then(() => {
+      this.resetForm();
+    });
+  }
+
+  EditarHeader(header: Header) {
+    this.isEditing = true;
+    this.editingId = header.id || null;
+    this.myHeader = { ...header };
+    this.btnTxt = 'Actualizar';
+  }
+
+  ActualizarHeader() {
+    if (!this.editingId) return;
+    this.headerService.updateHeader(this.editingId, this.myHeader).then(() => {
+      this.resetForm();
+    });
+  }
+
+  AgregarOActualizar() {
+    if (this.isEditing) {
+      this.ActualizarHeader();
+    } else {
+      this.AgregarHeader();
+    }
+  }
+
+  deleteHeader(id?: string) {
+    if (!id) return;
+    this.headerService.deleteHeader(id).then(() => {
+      console.log('Header eliminado correctamente!');
+    });
+  }
+
+  resetForm() {
+    this.myHeader = {
+      email: '',
+      goalLife: '',
+      location: '',
+      name: '',
+      phoneNumber: '',
+      photoUrl: '',
+      socialNetwork: ''
+    };
+    this.isEditing = false;
+    this.editingId = null;
+    this.btnTxt = 'Agregar';
+  }
 }
